3d60b6ed5b8a817820e0a0031f643fe4a2b50825
Mocha <84698300+Mocha1530@users.noreply.github.com>
2025-09-03 02:38:27 +0800

    Update gag.lua

--- gag.lua
+++ gag.lua
@@ -82,6 +82,7 @@ local InfoTab = Window:Tab("Info")
 local teleport = PlayerGui:FindFirstChild("Teleport_UI")
 local frame = teleport:FindFirstChild("Frame")
 
+-- Event teleport button UI
 local eventButton = Instance.new("ImageButton")
 eventButton.Name = "Event"
 eventButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
@@ -135,6 +136,124 @@ uiStroke.Color = Color3.fromRGB(40, 20, 11)
 uiStroke.Thickness = 2
 uiStroke.Parent = txt
 
+-- Gear teleport button UI
+local gearButton = Instance.new("ImageButton")
+gearButton.Name = "Gear"
+gearButton.BackgroundColor3 = Color3.fromRGB(97, 226, 51)
+gearButton.BackgroundTransparency = 0
+gearButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
+gearButton.BorderSizePixel = 0
+gearButton.LayoutOrder = 0
+gearButton.Position = UDim2.new(0.688038409, 0, 0.114772804, 0)
+gearButton.Size = UDim2.new(0.259808183, 0, 0.790713012, 0)
+gearButton.ZIndex = 1
+gearButton.ScaleType = Enum.ScaleType.Fit
+gearButton.Image = "rbxassetid://0"
+gearButton.HoverImage = "rbxassetid://0"
+gearButton.Parent = frame
+
+local gearAspectRatio = Instance.new("UIAspectRatioConstraint")
+gearAspectRatio.AspectRatio = 2.8125
+gearAspectRatio.AspectType = Enum.AspectType.FitWithinMaxSize
+gearAspectRatio.DominantAxis = Enum.DominantAxis.Width
+gearAspectRatio.Parent = gearButton
+
+local gearUIStroke = Instance.new("UIStroke")
+gearUIStroke.Color = Color3.fromRGB(80, 184, 42)
+gearUIStroke.Thickness = 1.5
+gearUIStroke.Parent = gearButton
+
+local geartxt = Instance.new("TextLabel")
+geartxt.Name = "Txt"
+geartxt.AnchorPoint = Vector2.new(0.5, 0.5)
+geartxt.BackgroundColor3 = Color3.fromRGB(76, 52, 35)
+geartxt.BackgroundTransparency = 1
+geartxt.BorderColor3 = Color3.fromRGB(0, 0, 0)
+geartxt.BorderSizePixel = 0
+geartxt.Position = UDim2.new(0.5, 0, 0.5, 0)
+geartxt.Size = UDim2.new(0.86500001, 0, 0.86500001, 0)
+geartxt.ZIndex = 1
+geartxt.FontFace = Font.new(
+    "rbxasset://fonts/families/ComicNeueAngular.json",  
+    Enum.FontWeight.Bold,
+    Enum.FontStyle.Normal
+)
+geartxt.Text = "GEAR"
+geartxt.TextColor3 = Color3.fromRGB(255, 255, 255)
+geartxt.TextScaled = true
+geartxt.TextSize = 14
+geartxt.TextWrapped = true
+geartxt.Parent = gearButton
+
+local gearSizeConstraint = Instance.new("UISizeConstraint")
+gearSizeConstraint.MaxSize = Vector2.new(math.huge, math.huge)
+gearSizeConstraint.MinSize = Vector2.new(50, 20)
+gearSizeConstraint.Parent = geartxt
+
+local gearUIStroke1 = Instance.new("UIStroke")
+gearUIStroke1.Color = Color3.fromRGB(80, 184, 42)
+gearUIStroke1.Thickness = 2
+gearUIStroke1.Parent = geartxt
+
+-- Pet teleport button UI
+local petButton = Instance.new("ImageButton")
+petButton.Name = "Pets"
+petButton.BackgroundColor3 = Color3.fromRGB(226, 163, 37)
+petButton.BackgroundTransparency = 0
+petButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
+petButton.BorderSizePixel = 0
+petButton.LayoutOrder = 1
+petButton.Position = UDim2.new(0.688038409, 0, 0.114772804, 0)
+petButton.Size = UDim2.new(0.259808183, 0, 0.790713012, 0)
+petButton.ZIndex = 1
+petButton.ScaleType = Enum.ScaleType.Fit
+petButton.Image = "rbxassetid://0"
+petButton.HoverImage = "rbxassetid://0"
+petButton.Parent = frame
+
+local petAspectRatio = Instance.new("UIAspectRatioConstraint")
+petAspectRatio.AspectRatio = 2.8125
+petAspectRatio.AspectType = Enum.AspectType.FitWithinMaxSize
+petAspectRatio.DominantAxis = Enum.DominantAxis.Width
+petAspectRatio.Parent = petButton
+
+local petUIStroke = Instance.new("UIStroke")
+petUIStroke.Color = Color3.fromRGB(166, 120, 27)
+petUIStroke.Thickness = 1.5
+petUIStroke.Parent = petButton
+
+local pettxt = Instance.new("TextLabel")
+pettxt.Name = "Txt"
+pettxt.AnchorPoint = Vector2.new(0.5, 0.5)
+pettxt.BackgroundColor3 = Color3.fromRGB(76, 52, 35)
+pettxt.BackgroundTransparency = 1
+pettxt.BorderColor3 = Color3.fromRGB(0, 0, 0)
+pettxt.BorderSizePixel = 0
+pettxt.Position = UDim2.new(0.5, 0, 0.5, 0)
+pettxt.Size = UDim2.new(0.86500001, 0, 0.86500001, 0)
+pettxt.ZIndex = 1
+pettxt.FontFace = Font.new(
+    "rbxasset://fonts/families/ComicNeueAngular.json",  
+    Enum.FontWeight.Bold,
+    Enum.FontStyle.Normal
+)
+pettxt.Text = "PETS"
+pettxt.TextColor3 = Color3.fromRGB(255, 255, 255)
+pettxt.TextScaled = true
+pettxt.TextSize = 14
+pettxt.TextWrapped = true
+pettxt.Parent = petButton
+
+local petSizeConstraint = Instance.new("UISizeConstraint")
+petSizeConstraint.MaxSize = Vector2.new(math.huge, math.huge)
+petSizeConstraint.MinSize = Vector2.new(50, 20)
+petSizeConstraint.Parent = pettxt
+
+local petUIStroke1 = Instance.new("UIStroke")
+petUIStroke1.Color = Color3.fromRGB(166, 120, 27)
+petUIStroke1.Thickness = 2
+petUIStroke1.Parent = pettxt
+
 local LocalPlayer = Players.LocalPlayer
 local Character = LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait()
 local HumanoidRootPart = Character:WaitForChild("HumanoidRootPart")
@@ -142,6 +261,22 @@ local Tp_Points = Workspace:FindFirstChild("Tutorial_Points")
 
 eventButton.MouseButton1Click:Connect(function()
     local teleportPoint = Tp_Points.Event_Point
+    
+    if teleportPoint then
+        HumanoidRootPart.CFrame = teleportPoint.CFrame
+    end
+end)
+
+gearButton.MouseButtonCick:Connect(function()
+    local teleportPoint = Tp_Points.Tutorial_Point_3
+    
+    if teleportPoint then
+        HumanoidRootPart.CFrame = teleportPoint.CFrame
+    end
+end)
+
+petButton.MouseButtonCick:Connect(function()
+    local teleportPoint = Tp_Points.Tutorial_Point_4
     
     if teleportPoint then
         HumanoidRootPart.CFrame = teleportPoint.CFrame
