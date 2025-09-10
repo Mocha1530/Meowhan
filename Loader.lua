1d636a68b32d381b4e5a62752667de12bb344d1e
Mocha <84698300+Mocha1530@users.noreply.github.com>
2025-09-11 04:03:49 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,9 +1,9 @@
 local Games = {
-  [126884695634066] = "https://raw.githubusercontent.com/Mocha1530/Meowhan/main/gag.lua"
+  [126884695634066] = "https://raw.githubusercontent.com/Mocha1530/Meowhan/refs/heads/main/gag.lua"
 }
 local Url = Games[game.PlaceId]
 if Url then 
   loadstring(game:HttpGet(Url, true))()
 else
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/Mocha1530/Meowhan/main/Global.lua", true))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Mocha1530/Meowhan/refs/heads/main/Global.lua", true))()
 end
