8b50617a0099860ea1e9a291fe086c6bad2414a0
Mocha <84698300+Mocha1530@users.noreply.github.com>
2025-09-11 23:19:56 +0800

    Create fruitmutations.lua

---
+++ gag/data/fruitmutations.lua
@@ -0,0 +1,484 @@
+return {
+    mutations = {
+        {
+            mutation_id = "cyclonic",
+            display_name = "Cyclonic",
+            multiplier = 50
+        },
+        {
+            mutation_id = "voidtouched",
+            display_name = "Voidtouched",
+            multiplier = 135
+        },
+        {
+            mutation_id = "bloodlit",
+            display_name = "Bloodlit",
+            multiplier = 4
+        },
+        {
+            mutation_id = "stormcharged",
+            display_name = "Stormcharged",
+            multiplier = 180
+        },
+        {
+            mutation_id = "ancientamber",
+            display_name = "AncientAmber",
+            multiplier = 50
+        },
+        {
+            mutation_id = "infernal",
+            display_name = "Infernal",
+            multiplier = 180
+        },
+        {
+            mutation_id = "honeyglazed",
+            display_name = "HoneyGlazed",
+            multiplier = 5
+        },
+        {
+            mutation_id = "paradisal",
+            display_name = "Paradisal",
+            multiplier = 100
+        },
+        {
+            mutation_id = "tempestuous",
+            display_name = "Tempestuous",
+            multiplier = 12
+        },
+        {
+            mutation_id = "heavenly",
+            display_name = "Heavenly",
+            multiplier = 5
+        },
+        {
+            mutation_id = "cracked",
+            display_name = "Cracked",
+            multiplier = 4
+        },
+        {
+            mutation_id = "corruptfoxfirechakra",
+            display_name = "CorruptFoxfireChakra",
+            multiplier = 90
+        },
+        {
+            mutation_id = "spaghetti",
+            display_name = "Spaghetti",
+            multiplier = 15
+        },
+        {
+            mutation_id = "plasma",
+            display_name = "Plasma",
+            multiplier = 5
+        },
+        {
+            mutation_id = "aurora",
+            display_name = "Aurora",
+            multiplier = 90
+        },
+        {
+            mutation_id = "pasta",
+            display_name = "Pasta",
+            multiplier = 3
+        },
+        {
+            mutation_id = "ascendedchakra",
+            display_name = "AscendedChakra",
+            multiplier = 230
+        },
+        {
+            mutation_id = "sauce",
+            display_name = "Sauce",
+            multiplier = 3
+        },
+        {
+            mutation_id = "enchanted",
+            display_name = "Enchanted",
+            multiplier = 50
+        },
+        {
+            mutation_id = "luminous",
+            display_name = "Luminous",
+            multiplier = 50
+        },
+        {
+            mutation_id = "amber",
+            display_name = "Amber",
+            multiplier = 10
+        },
+        {
+            mutation_id = "boil",
+            display_name = "Boil",
+            multiplier = 15
+        },
+        {
+            mutation_id = "enlightened",
+            display_name = "Enlightened",
+            multiplier = 35
+        },
+        {
+            mutation_id = "jackpot",
+            display_name = "Jackpot",
+            multiplier = 15
+        },
+        {
+            mutation_id = "drenched",
+            display_name = "Drenched",
+            multiplier = 5
+        },
+        {
+            mutation_id = "cooked",
+            display_name = "Cooked",
+            multiplier = 10
+        },
+        {
+            mutation_id = "eclipsed",
+            display_name = "Eclipsed",
+            multiplier = 20
+        },
+        {
+            mutation_id = "sliced",
+            display_name = "Sliced",
+            multiplier = 50
+        },
+        {
+            mutation_id = "toxic",
+            display_name = "Toxic",
+            multiplier = 15
+        },
+        {
+            mutation_id = "tranquil",
+            display_name = "Tranquil",
+            multiplier = 20
+        },
+        {
+            mutation_id = "ceramic",
+            display_name = "Ceramic",
+            multiplier = 32
+        },
+        {
+            mutation_id = "alienlike",
+            display_name = "Alienlike",
+            multiplier = 100
+        },
+        {
+            mutation_id = "shocked",
+            display_name = "Shocked",
+            multiplier = 100
+        },
+        {
+            mutation_id = "touchdown",
+            display_name = "Touchdown",
+            multiplier = 105
+        },
+        {
+            mutation_id = "foxfirechakra",
+            display_name = "FoxfireChakra",
+            multiplier = 90
+        },
+        {
+            mutation_id = "blitzshock",
+            display_name = "Blitzshock",
+            multiplier = 50
+        },
+        {
+            mutation_id = "burnt",
+            display_name = "Burnt",
+            multiplier = 4
+        },
+        {
+            mutation_id = "disco",
+            display_name = "Disco",
+            multiplier = 125
+        },
+        {
+            mutation_id = "meatball",
+            display_name = "Meatball",
+            multiplier = 3
+        },
+        {
+            mutation_id = "sundried",
+            display_name = "Sundried",
+            multiplier = 85
+        },
+        {
+            mutation_id = "cloudtouched",
+            display_name = "Cloudtouched",
+            multiplier = 5
+        },
+        {
+            mutation_id = "wiltproof",
+            display_name = "Wiltproof",
+            multiplier = 4
+        },
+        {
+            mutation_id = "glimmering",
+            display_name = "Glimmering",
+            multiplier = 2
+        },
+        {
+            mutation_id = "corruptchakra",
+            display_name = "CorruptChakra",
+            multiplier = 15
+        },
+        {
+            mutation_id = "gnomed",
+            display_name = "Gnomed",
+            multiplier = 15
+        },
+        {
+            mutation_id = "aromatic",
+            display_name = "Aromatic",
+            multiplier = 3
+        },
+        {
+            mutation_id = "windstruck",
+            display_name = "Windstruck",
+            multiplier = 2
+        },
+        {
+            mutation_id = "brainrot",
+            display_name = "Brainrot",
+            multiplier = 100
+        },
+        {
+            mutation_id = "flaming",
+            display_name = "Flaming",
+            multiplier = 25
+        },
+        {
+            mutation_id = "subzero",
+            display_name = "Subzero",
+            multiplier = 40
+        },
+        {
+            mutation_id = "junkshock",
+            display_name = "Junkshock",
+            multiplier = 45
+        },
+        {
+            mutation_id = "wet",
+            display_name = "Wet",
+            multiplier = 2
+        },
+        {
+            mutation_id = "abyssal",
+            display_name = "Abyssal",
+            multiplier = 240
+        },
+        {
+            mutation_id = "harmonisedchakra",
+            display_name = "HarmonisedChakra",
+            multiplier = 35
+        },
+        {
+            mutation_id = "gloom",
+            display_name = "Gloom",
+            multiplier = 30
+        },
+        {
+            mutation_id = "acidic",
+            display_name = "Acidic",
+            multiplier = 15
+        },
+        {
+            mutation_id = "maelstrom",
+            display_name = "Maelstrom",
+            multiplier = 100
+        },
+        {
+            mutation_id = "rot",
+            display_name = "Rot",
+            multiplier = 8
+        },
+        {
+            mutation_id = "chilled",
+            display_name = "Chilled",
+            multiplier = 2
+        },
+        {
+            mutation_id = "bloom",
+            display_name = "Bloom",
+            multiplier = 8
+        },
+        {
+            mutation_id = "fortune",
+            display_name = "Fortune",
+            multiplier = 50
+        },
+        {
+            mutation_id = "verdant",
+            display_name = "Verdant",
+            multiplier = 4
+        },
+        {
+            mutation_id = "moonlit",
+            display_name = "Moonlit",
+            multiplier = 2
+        },
+        {
+            mutation_id = "sandy",
+            display_name = "Sandy",
+            multiplier = 3
+        },
+        {
+            mutation_id = "infected",
+            display_name = "Infected",
+            multiplier = 75
+        },
+        {
+            mutation_id = "chakra",
+            display_name = "Chakra",
+            multiplier = 15
+        },
+        {
+            mutation_id = "lightcycle",
+            display_name = "Lightcycle",
+            multiplier = 50
+        },
+        {
+            mutation_id = "clay",
+            display_name = "Clay",
+            multiplier = 5
+        },
+        {
+            mutation_id = "cosmic",
+            display_name = "Cosmic",
+            multiplier = 240
+        },
+        {
+            mutation_id = "corrupt",
+            display_name = "Corrupt",
+            multiplier = 20
+        },
+        {
+            mutation_id = "celestial",
+            display_name = "Celestial",
+            multiplier = 120
+        },
+        {
+            mutation_id = "twisted",
+            display_name = "Twisted",
+            multiplier = 5
+        },
+        {
+            mutation_id = "glitched",
+            display_name = "Glitched",
+            multiplier = 85
+        },
+        {
+            mutation_id = "harmonisedfoxfirechakra",
+            display_name = "HarmonisedFoxfireChakra",
+            multiplier = 190
+        },
+        {
+            mutation_id = "molten",
+            display_name = "Molten",
+            multiplier = 25
+        },
+        {
+            mutation_id = "dawnbound",
+            display_name = "Dawnbound",
+            multiplier = 150
+        },
+        {
+            mutation_id = "galactic",
+            display_name = "Galactic",
+            multiplier = 120
+        },
+        {
+            mutation_id = "blazing",
+            display_name = "Blazing",
+            multiplier = 52
+        },
+        {
+            mutation_id = "fried",
+            display_name = "Fried",
+            multiplier = 8
+        },
+        {
+            mutation_id = "zombified",
+            display_name = "Zombified",
+            multiplier = 25
+        },
+        {
+            mutation_id = "pollinated",
+            display_name = "Pollinated",
+            multiplier = 3
+        },
+        {
+            mutation_id = "oldamber",
+            display_name = "OldAmber",
+            multiplier = 20
+        },
+        {
+            mutation_id = "frozen",
+            display_name = "Frozen",
+            multiplier = 10
+        },
+        {
+            mutation_id = "meteoric",
+            display_name = "Meteoric",
+            multiplier = 125
+        },
+        {
+            mutation_id = "warped",
+            display_name = "Warped",
+            multiplier = 75
+        },
+        {
+            mutation_id = "corrosive",
+            display_name = "Corrosive",
+            multiplier = 40
+        },
+        {
+            mutation_id = "friendbound",
+            display_name = "Friendbound",
+            multiplier = 70
+        },
+        {
+            mutation_id = "oil",
+            display_name = "Oil",
+            multiplier = 15
+        },
+        {
+            mutation_id = "radioactive",
+            display_name = "Radioactive",
+            multiplier = 55
+        },
+        {
+            mutation_id = "static",
+            display_name = "Static",
+            multiplier = 8
+        },
+        {
+            mutation_id = "choc",
+            display_name = "Choc",
+            multiplier = 2
+        },
+        {
+            mutation_id = "beanbound",
+            display_name = "Beanbound",
+            multiplier = 100
+        },
+        {
+            variant_id = "normal",
+            display_name = "Normal",
+            multiplier = 1
+        },
+        {
+            variant_id = "silver",
+            display_name = "Silver",
+            multiplier = 5
+        },
+        {
+            variant_id = "gold",
+            display_name = "Gold",
+            multiplier = 20
+        },
+        {
+            variant_id = "rainbow",
+            display_name = "Rainbow",
+            multiplier = 50
+        }
+    }
+}
