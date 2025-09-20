883b2dc578f9d19588f32f97f9cbecdfbf5323c8
Mocha <84698300+Mocha1530@users.noreply.github.com>
2025-09-21 00:27:18 +0800

    Update gag.lua

--- gag.lua
+++ gag.lua
@@ -77,7 +77,7 @@ for k_a_c, _ in pairs(a_c_data["Cosmetic Crates"]) do
     table.insert(a_c_list.crates, k_a_c)
 end
 
-local a_e_s_data = loadstring(game:HttpGet("https://raw.githubusercontent.com/Mocha1530/Meowhan/refs/heads/main/gag/data/EventShopData.lua", true))() --[[EventShopData]]
+local a_e_s_data = EventShopData --[[loadstring(game:HttpGet("https://raw.githubusercontent.com/Mocha1530/Meowhan/refs/heads/main/gag/data/EventShopData.lua", true))()]]
 local a_e_s_list = {
     seed = {},
     gear = {},
@@ -597,7 +597,7 @@ local function findFruit(filters)
     local function checkFruit(fruit)
         local Age = fruit:FindFirstChild("Age", true)
         local MaxAge = fruit:GetAttribute("MaxAge")
-        if (Age and Age:IsA("NumberVaue") and Age.Value ~= MaxAge then
+        if (Age and Age:IsA("NumberVaue")) and Age.Value ~= MaxAge then
             return false
         end
             
@@ -1186,7 +1186,7 @@ end
                             for shop, items in pairs(controller.selectedItems) do
                                 for _, itemName in ipairs(items) do
                                     local item = controller.stock[itemName]
-                                    table.insert(itemsToBuy, {name = itemName, count = item.StockAmount, shopIndex = item.ShopIndex})
+                                    table.insert(itemsToBuy, {name = itemName, count = item.StockAmount[2], shopIndex = item.ShopIndex})
                                 end
                             end
                         end
@@ -1197,7 +1197,7 @@ end
                                     table.insert(itemsToBuy, {name = itemName, count = count})
                                 end
                             else
-                                table.insert(itemsToBuy, {name = itemName, count = count.StockAmount, shopIndex = count.ShopIndex})
+                                table.insert(itemsToBuy, {name = itemName, count = count.StockAmount[2], shopIndex = count.ShopIndex})
                             end
                         end
                     end
@@ -2882,7 +2882,7 @@ local StatsSection = InfoTab:Section("Session Statistics")
 
 -- About
 AboutSection:Label("Meowhan Grow A Garden Exploit")
-AboutSection:Label("Version: 1.3.252")
+AboutSection:Label("Version: 1.3.251")
 
 -- Stats
 local GameInfo = MarketplaceService:GetProductInfo(game.PlaceId)
